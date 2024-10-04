d6a7313ed172413c4e840ba1b0f11de40e8b9e59
septimusforster <septimusforster@gmail.com>
2024-10-04 14:54:24 +0100

    quiz HTML CSS complete

--- dist/student/src/quiz.js
+++ dist/student/src/quiz.js
@@ -1,4 +1,5 @@
 // document.querySelector('#startup-dg').show();
+document.querySelector('#submitted-dg').show();
 
 const infoBtn = document.querySelector('#info-btn');
 const oculus = document.querySelector('button.oculus');
@@ -7,7 +8,13 @@ const submitBtn = document.querySelector('#submit-btn');
 const pasteBtn = document.querySelector('#paste-btn');
 const txtCode = document.getElementById('txtcode');
 const dg0btns = document.querySelectorAll('.dg0btn');
-const closeBtns = document.querySelectorAll('.close_dg');
+const closeBtns = document.querySelectorAll('button.ex');
+const yesBtn = document.querySelector('#oi-btns > button:nth-child(2)');
+const resultBtn = document.querySelector('#submitted-dg button');
+const progressbar = document.querySelector('.progressbar');
+const scorebar = document.querySelector('#score-dg > div > div:nth-child(3)');
+
+const scoreDialog = document.querySelector('#score-dg');
 
 //closing dialogs
 closeBtns.forEach(btn => {
@@ -15,6 +22,12 @@ closeBtns.forEach(btn => {
         document.querySelectorAll('dialog').forEach(dia => {
             if (dia.contains(e.target)) {
                 dia.close();
+            }
+            if (dia.id == 'score-dg') {
+                progressbar.setAttribute('aria-value', '0');
+                progressbar.style.setProperty("--progress", '0');
+                progressbar.toggleAttribute('data-bullseye');
+                scorebar.classList.remove('sco');
             }
         });
     });
@@ -47,10 +60,50 @@ dg0btns[1].addEventListener('click', (e) => {
     e.target.disabled = true;
     dg0btns[1].classList.add('clk');
 });
-//submit test btn
+//submit test request btn
 submitBtn.addEventListener('click', () => {
     document.querySelector('dialog#subreq-dg').showModal();
 });
+//submit YES btn
+yesBtn.addEventListener('click', (e) => {
+    const par = e.target.parentElement;
+    const chdrn = [...par.children];
+    chdrn.forEach(ch => ch.disabled = true);
+    yesBtn.classList.add('clk');
+});
+//result btn
+const numOfQuestions = 100;
+const score = 70;
+scorebar.textContent = `${score} / ${numOfQuestions}`;
+const cent = score / numOfQuestions * 100;
+let scorePercent = parseInt(cent.toFixed());
+resultBtn.addEventListener('click', (e) => {
+    scoreDialog.showModal();
+    let progress = 0;
+
+    var intervalID = setInterval(() => {
+        progress += 1;
+        updateprogress(progress);
+        if (progress == scorePercent) {
+            clearInterval(intervalID);
+            progressbar.toggleAttribute('data-bullseye');
+            scorebar.classList.add('sco');
+        }
+    }, 50);
+});
+
+function updateprogress(progress) {
+    progressbar.setAttribute('aria-value', progress);
+    progressbar.style.setProperty("--progress", progress + "%");
+}
+
+
+
+
+
+
+
+
 /*
 const asideP = document.querySelectorAll('aside > p');
 asideP.forEach(p => {
