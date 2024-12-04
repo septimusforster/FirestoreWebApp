3aaea63bc99662a106e02f8edd0cf03a23927c34
septimusforster <septimusforster@gmail.com>
2024-12-04 12:54:30 +0100

    robotics done

--- form/form.js
+++ form/form.js
@@ -1,5 +1,5 @@
 import { initializeApp, deleteApp } from "firebase/app";
-import { getFirestore, collection, getDoc, doc, query, where, getDocs, limit } from "firebase/firestore";
+import { getFirestore, collection, getDoc, doc, query, where, getDocs, limit, setDoc } from "firebase/firestore";
 import  configs from "../src/JSON/configurations.json" assert {type: 'json'};
 
 var app = initializeApp(configs[6]);
@@ -22,13 +22,15 @@ let counter = 0;
 const notice = document.querySelector('.notice');
 const robotform = document.forms.namedItem('robotform');
 const robotbody = robotform.querySelector('.body');
+const dialog = document.querySelectorAll('dialog');
 
 class Student {
     constructor(_id, _name, _admNo, _dob) {
         this.id = _id,
         this.name = _name,
         this.admNo = _admNo,
-        this.dob = _dob
+        this.dob = _dob,
+        this.file = null
     }
     
     insertExpectations () {
@@ -50,21 +52,80 @@ class Student {
             </div>    
         `)
         counter++;
-        console.log(counter);
+        console.log('counter', counter);
     }
 
+    insertPaymentDetails () {
+        robotbody.insertAdjacentHTML('beforeend', `
+            <div class="fmgp">
+                <div id="pop">Payment Details</div>
+                <div>
+                    <small>Account Name</small><strong>Destiny Christian Academy</strong>
+                    <small>Bank Name</small><strong>Zenith Bank</strong>
+                    <small>Account Number</small><strong>1012766749</strong>
+                </div>
+                <div>
+                    <label for="uploadpop">Upload Receipt</label>
+                    <input type="file" name="uploadpop" id="uploadpop">
+                    <output></output>
+                </div>
+            </div>
+        `);
+        robotbody.querySelector('input[type="file"]').addEventListener('input', (e) => {
+            const f = e.target.files[0];
+            //convert to base64
+            const fr = new FileReader();
+            fr.onload = () => {
+                this.file = fr.result;
+            }
+            fr.readAsDataURL(f);
+            e.target.nextElementSibling.textContent = f.name;
+        });
+        counter++;
+        console.log('counter', counter);
+    }
 }
-
+let student;
 robotform.addEventListener('submit', async (e) => {
     e.preventDefault();
     e.submitter.disabled = true;
     e.submitter.classList.add('clk');
+    //scroll robotbody
+    // const height = robotbody.scrollHeight;
+    // robotbody.scrollBy(0, height);
     const cfg_no = configs[7].indexOf(clsName.value);
     const admission = admNo.value.replaceAll(' ', '').toUpperCase();
 
-    if (counter === 1) {
-
+    if (counter === 1) { //get proof of payment for upload
+        const inputs = [...robotbody.querySelectorAll('input[type="checkbox"]:checked, textarea')].map(input => input.tagName.toLowerCase() == 'textarea' ? input.value : input.nextElementSibling.textContent);
+        console.log(student.id);
+        await setDoc(doc(db, 'robotics', student.id), {expectations: inputs}, {merge: true})
+        robotbody.querySelectorAll('input, select, textarea').forEach(elem => {
+            elem.disabled = true;
+            elem.toggleAttribute('readonly', true);
+        });
+        student.insertPaymentDetails();
+        e.submitter.classList.remove('clk');
+        e.submitter.disabled = false;
         return;
+    }
+    if (counter === 2) {
+        // e.submitter.disabled = false;
+        if (!student.file) {
+            notify('No file selected.');
+            e.submitter.classList.remove('clk');
+            e.submitter.disabled = false;
+            return;
+        } else {
+            //store base64 in robotics
+            await setDoc(doc(db, 'robotics', student.id), {file: student.file, isAcknowledged: true, dateTime: Date.now()}, {merge: true});
+            //display acknowledgement form as show
+            robotform.style.display = 'none';
+            dialog[0].show();
+            return;
+            // e.submitter.classList.remove('clk');
+            // e.submitter.disabled = false;
+        }
     }
     chooseConfig(cfg_no);
     const q1 = query(collection(db, 'robotics'), where('admNo', '==', admission), limit(1));
@@ -81,17 +142,18 @@ robotform.addEventListener('submit', async (e) => {
         } else {
             const found = snap2.docs[0].data();
             const dob = found.dob ? Intl.DateTimeFormat('en-GB', {dateStyle: 'long'}).format(new Date(found.dob)) : 'unknown';
-            const student = new Student(
+            student = new Student(
                 found.id,
                 `${found.last_name} ${found.first_name}`,
                 found.admission_no,
                 dob,
             );
-            // await setDoc(doc(db, 'robotics', student.id), {
-            //     admNo: student.admNo,
-            //     fullname: student.name,
-            // });
-            robotform.querySelectorAll('input, select, textarea').forEach(elem => {
+            await setDoc(doc(db, 'robotics', student.id), {
+                admNo: student.admNo,
+                fullname: student.name,
+                isAcknowledged: false
+            });
+            robotbody.querySelectorAll('input, select, textarea').forEach(elem => {
                 elem.disabled = true;
                 elem.toggleAttribute('readonly', true);
             });
@@ -100,7 +162,14 @@ robotform.addEventListener('submit', async (e) => {
             e.submitter.disabled = false;
         }
     } else {
-        // snap1.docs[0].data
+        const found1 = snap1.docs[0].data();
+        const today = Intl.DateTimeFormat('en-GB', {dateStyle: 'full'}).format(Date.now());
+        const otherDay = Intl.DateTimeFormat('en-GB', {dateStyle: 'full'}).format(found1.dateTime);
+        if (found1?.isAcknowledged) {
+            dialog[1].querySelector('p:first-of-type').textContent = `Your payment ${today == otherDay ? 'was submitted earlier today.' : 'was submitted on ' + otherDay + '.'}`;
+            robotform.style.display = 'none';
+            dialog[1].show();
+        }
     }
 
     //if counter = 0, look for student in the backend and discover if he has already made payment
